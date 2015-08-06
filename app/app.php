c8c166e3e467e993c4755eb44647a501703a7883
Jason Bethel & Kelli Sommerdyke <student@epicodus.com>
2015-08-06 10:25:02 -0700

    add car dealership form twig

--- app/app.php
+++ app/app.php
@@ -3,32 +3,12 @@
     require_once __DIR__ . '/../src/Car.php';
 
     $app = new Silex\Application();
-    $app->get("/", function() {
-        return "
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
-                <title>Find a Car</title>
-            </head>
-            <body>
-                <div class='container'>
-                    <h1>Find a Car!</h1>
-                    <form action='/car_results'>
-                        <div class='form-group'>
-                            <label for='price'>Enter Maximum Price:</label>
-                            <input id='price' name='price' class='form-control'type='number'>
-                        </div>
-                        <div class='form-group'>
-                            <label for='miles'>Enter Maximum Mileage:</label>
-                            <input id='miles' name='miles' class='form-control' type='number'>
-                        </div>
-                        <button type='submit' class='btn-success'>Submit</button>
-                    </form>
-                </div>
-            </body>
-            </html>
-            ";
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function()  use ($app) {
+        return $app['twig']->render('cars.html.twig');
     });
 
     $app->get("/car_results", function() {
